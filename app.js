a72be5d335990cc4f617384531f5e90181bbae05
anank0805kr <112902400+anank0805kr@users.noreply.github.com>
2022-09-09 18:57:34 +0530

    initial comment

---
+++ app.js
@@ -0,0 +1,13 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textinput = document.querySelector("#text-input")
+
+console.log(textinput);
+
+function clickEventHandler() {
+    console.log("clicked!")
+    console.log("input", textinput.value);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler)
+
+// var btn = document.querySelector("#btn-translates");
